f59939be9996cf57f7cccc60087de2a19d459d5e
Instillingererno <sveinungskole@gmail.com>
2017-09-23 00:41:01 +0200

    asdfs

---
+++ Ovelse7/Oppgave1.java
@@ -0,0 +1,24 @@
+//Oppgave 9.11.2 s.314 22-09-2017
+
+import java.util.Arrays;
+
+class Temperaturer {
+    int[][] temps = new int[3][3];
+
+    public gjennomsnittForHverDag() {
+        int[] svar = new int[temps.length];
+        for(int i = 0; i < temps.length; i++) {
+            
+        }
+        //return tabell med verdi for hver dag
+    }
+    public gjennomsnittForHverTime( /* input dag */) {
+
+    }
+    public gjennomsnittForHeleMandeden() {
+
+    }
+    public gruppertGjennomsnitt() {
+
+    }
+}
