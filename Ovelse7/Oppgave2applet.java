ff8252968041531ba94d302282c78a6daf40cab1
Instillingererno <sveinungskole@gmail.com>
2017-09-30 16:36:58 +0200

    asdfgtr

--- Ovelse7/Oppgave2applet.java
+++ Ovelse7/Oppgave2applet.java
@@ -1,16 +1,10 @@
-//Oppgave 10.9.1 s.345 29-09-2017
-import java.awt.Graphics;
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+//Oppgave 10.9.1 appletversjon s.345 29-09-2017
+import javax.swing.*;
+import java.awt.*;
 
-class Vindu extends JFrame {
-    public Vindu(String tittel) {
-        setTitle(tittel);
-        setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Tegning tegningen = new Tegning();
-        add(tegningen);
+public class Oppgave2applet extends JApplet {
+    public void init() {
+        add(new Tegning());
     }
 }
 
@@ -26,12 +20,5 @@ class Tegning extends JPanel {
         tegneflate.fillOval(170, 200, 30, 30);
         tegneflate.drawArc(150, 160, getWidth()-200-getWidth()/5, getHeight()-200-getHeight()/5, -30, -120);
 
-    }
-}
-
-class Oppgave2 {
-    public static void main(String[] args) {
-        Vindu etVindu = new Vindu("Enkel grafikk");
-        etVindu.setVisible(true);
     }
 }
