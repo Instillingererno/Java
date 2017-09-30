9344df69bc6fe8c66c5991336ed151585b5540fe
Instillingererno <sveinungskole@gmail.com>
2017-09-30 17:48:37 +0200

    oiasjasd

--- Ovelse8/Oppgave1.java
+++ Ovelse8/Oppgave1.java
@@ -1,1 +1,20 @@
 //Oppgave 8.16.1 s.283 30-09-2017
+import javax.swing.*;
+
+class NyString {
+    private String data = "";
+}
+
+class Oppgave1 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Test");
+                frame.setSize(500,500);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+
+    }
+}
