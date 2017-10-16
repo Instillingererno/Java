1c6b27227839b94e63c710a5173f3a7ca38bc7f6
Instillingererno <sveinungskole@gmail.com>
2017-10-16 15:44:41 +0200

    asdf

--- Ovelse1GK/TommerTilCent.java
+++ Ovelse1GK/TommerTilCent.java
@@ -3,14 +3,14 @@
 import static javax.swing.JOptionPane.*;
 
 class TommerTilCent {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		String tommerLest = showInputDialog("Tommer: ");
-		
+
 		double tommer = Double.parseDouble(tommerLest);
-		
+
 		double centimeter = tommer * 2.54;
-		
+
 		showMessageDialog(null, "Centimeter: " + centimeter);
-		
+
 	}
 }
