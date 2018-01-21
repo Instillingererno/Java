d803fb57246f9ad0fe963b7f4903d058e68b6513
Instillingererno <sveinungskole@gmail.com>
2018-01-21 19:55:15 +0100

    fds

--- DatabaseOvelser/src/Ovelse7/queries.sql
+++ DatabaseOvelser/src/Ovelse7/queries.sql
@@ -1,3 +1,5 @@
+USE ovelse7;
+
 SELECT oppdrag.* FROM oppdrag LEFT JOIN historikk ON oppdrag.oppdragsNr = historikk.oppdragsNr WHERE historikk.oppdragsNr IS NULL;
 
 INSERT INTO bedrift(navn, tlf, email) VALUES ('Apple', 12345, 'apple@apple.com');
@@ -5,4 +7,13 @@ INSERT INTO bedrift(navn, tlf, email) VALUES ('Apple', 12345, 'apple@apple.com')
 INSERT INTO kvalifikasjon(navn, beskrivelse) VALUES ('data', 'kan ting om data');
 
 INSERT INTO oppdrag(orgNr, idKvalifikasjon, startDato, sluttDato)
-    SELECT 1001010101010, idKvalifikasjon,
+    SELECT 1, idKvalifikasjon, '2018-01-19', '2018-01-22'
+        FROM kvalifikasjon WHERE navn = 'data'
+            LIMIT 1;
+
+INSERT INTO kvalifikasjon(navn, beskrivelse) VALUES ('stol', 'kan ting om stoler');
+
+INSERT INTO oppdrag(orgNr, idKvalifikasjon, startDato, sluttDato)
+    SELECT 1, idKvalifikasjon, '2018-01-19', '2018-01-22'
+    FROM kvalifikasjon WHERE navn = 'stol'
+    LIMIT 1;
