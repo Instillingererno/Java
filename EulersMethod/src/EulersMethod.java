fd221f4163f732df113670d57478a9716b67713d
L_Sveinung <sveinuov@stud.ntnu.no>
2018-04-04 22:14:26 +0200

    La til en del ting

---
+++ EulersMethod/src/EulersMethod.java
@@ -0,0 +1,38 @@
+public class EulersMethod {
+    private final Function function;
+    private final double knownX;
+    private final double knownY;
+
+    EulersMethod(Function function, double knownX, double knownY) {
+        this.function = function;
+        this.knownX = knownX;
+        this.knownY = knownY;
+    }
+
+    double estimateValue(double xToEstimate, double stepLength) {
+        int steps = (int) ((xToEstimate - knownX) / stepLength);
+        double lastYValue = knownY;
+        for(double i = 0; i < steps; i++) {
+            lastYValue = lastYValue + stepLength * function.run((knownX + stepLength * i), lastYValue);
+        }
+        return lastYValue;
+    }
+
+    public static void main(String[] args) {
+        EulersMethod test = new EulersMethod(((x, y) -> 6*Math.pow(x,2) - 3*Math.pow(x,2)*y), 0, 3);
+        System.out.printf("Steplength = 1:\t\t %.4f\n", test.estimateValue(1, 1));
+        System.out.printf("Steplength = 0.1:\t %.4f\n", test.estimateValue(1, 0.1));
+        System.out.printf("Steplength = 0.01:\t %.4f\n", test.estimateValue(1, 0.01));
+        System.out.printf("Steplength = 0.001:\t %.4f\n", test.estimateValue(1, 0.001));
+        System.out.println();
+        System.out.printf("Difference: %.4f\n", Math.abs(test.estimateValue(1, 1) - (2+Math.pow(Math.E,Math.pow(-1,3)))));
+        System.out.printf("Difference: %.4f\n", Math.abs(test.estimateValue(1, 0.1) - (2+Math.pow(Math.E,Math.pow(-1,3)))));
+        System.out.printf("Difference: %.4f\n", Math.abs(test.estimateValue(1, 0.01) - (2+Math.pow(Math.E,Math.pow(-1,3)))));
+        System.out.printf("Difference: %.4f\n", Math.abs(test.estimateValue(1, 0.001) - (2+Math.pow(Math.E,Math.pow(-1,3)))));
+    }
+}
+
+
+interface Function {
+    double run(double x, double y);
+}
--- Ovelse18/src/Ovelse18/Database.java
+++ Ovelse18/src/Ovelse18/Database.java
@@ -2,32 +2,157 @@ package Ovelse18;
 
 import com.mysql.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
     private final Connection tilkobling;
+    private Statement setning;
     private final String database = "jdbc:mysql://mysql.stud.iie.ntnu.no:3306/sveinuov";
     private final String brukernavn = "sveinuov";
     private final String passord = "YtDstNqL";
 
     public Database() throws SQLException {
-        new Driver();
         tilkobling = DriverManager.getConnection(database, brukernavn, passord);
+        startForbindelse();
     }
 
-    public void lukkTilkobling() {
+    public void startForbindelse() {
+        try {
+            new Driver();
+            setning = tilkobling.createStatement();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void lukkForbindelse() {
         try {
             if(tilkobling != null) tilkobling.close();
             else throw new Exception("Tilkobling finnes ikke, burde den?");
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean regNyBok(Bok nyBok) {
+        /*
+            Denne metoden skal registrere en ny tittel og samtidig eksemplar nummer 1 av denne tittelen.
+
+            SQL-setninger:
+
+            insert into boktittel(isbn, forfatter, tittel) values(<isbn>, <forfatter>, <tittel>)
+            insert into eksemplar(isbn, eks_nr) values(<isbn>, 1);
+
+            Metoden skal returnere false dersom bok med dette ISBN er registrert fra før. Da skal ingen oppdatering skje.
+         */
+
+        if(isbnEksisterer(nyBok.getIsbn())) {
+            return false;
+        } else {
+            try {
+                tilkobling.setAutoCommit(false);
+
+                String insert1 = "INSERT INTO boktittel(isbn, forfatter, tittel) VALUES('" + nyBok.getIsbn() + "','" + nyBok.getForfatter() + "','" + nyBok.getTittel() + "');";
+                String insert2 = "INSERT INTO eksemplar(isbn, eks_nr) VALUES('" + nyBok.getIsbn() + "', 1);";
+
+                if(setning.executeUpdate(insert1) != 0) {
+                    if(setning.executeUpdate(insert2) != 0) {
+                        tilkobling.commit();
+                        return true;
+                    } else {
+                        tilkobling.rollback();
+                        return false;
+                    }
+                } else {
+                    tilkobling.rollback();
+                    return false;
+                }
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    tilkobling.setAutoCommit(true);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isbnEksisterer(String isbn) {
+        try {
+            ResultSet resultSet = setning.executeQuery("SELECT * FROM boktittel WHERE isbn = '" + isbn + "';");
+            boolean eksisterer = false;
+            while(resultSet.next()) {
+                eksisterer = true;
+            }
+            if(eksisterer) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public int regNyttEksemplar(String isbn) {
+        if(isbnEksisterer(isbn)) {
+            int nyttNr = 1;
+            boolean ok = false;
+            ResultSet res = null;
+            PreparedStatement selectSetning;
+            PreparedStatement insertSetning;
+
+            while(!ok) {
+                String selectSQL = "SELECT MAX(eks_nr) AS maks FROM eksemplar WHERE isbn = '" + isbn + "';";
+                try {
+                    selectSetning = tilkobling.prepareStatement(selectSQL);
+                    res = selectSetning.executeQuery();
+
+                    res.next();
+                    nyttNr = res.getInt("maks") + 1;
+                    String insertSQL = "INSERT INTO eksemplar(isbn, eksnr) VALUES (?,?);";
+                    insertSetning = tilkobling.prepareStatement(insertSQL);
+                    insertSetning.setString(1, isbn);
+                    insertSetning.setInt(2, nyttNr);
+                    insertSetning.executeUpdate();
+                    ok = true;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                return nyttNr;
+            }
+        }
+        return 0;
+    }
+
+    public boolean lånUtEksemplar(String isbn, String navn, int eksnr) {
+        String updSetning = "UPDATE eksemplar SET laant_av = '" + navn + "' WHERE isbn = '" + isbn + "' AND eks_nr = '" + eksnr + "';";
+        try {
+            if(setning.executeUpdate(updSetning) != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
         }
     }
 
     public static void main(String[] agrs) throws SQLException {
-        new Database();
+        Database database = new Database();
+        database.regNyBok(new Bok("asdas", "asdasd", "asdasd22"));
+        database.regNyBok(new Bok("0-07-241163-5", "asdasd", "asdasd22"));
+        database.lånUtEksemplar("asdasd", "asdasd", 2);
+        database.lånUtEksemplar("0-07-241163-5", "asdasd", 20);
+        database.lånUtEksemplar("0-07-241163-5", "Ingunn Sund", 2);
+        System.out.println(database.regNyttEksemplar("0-596-00123-1"));
+        System.out.println(database.regNyBok(new Bok("0-596-00123-2", "Test shiiet", "Ingunn Sund")));
+        database.lukkForbindelse();
     }
 
 }
